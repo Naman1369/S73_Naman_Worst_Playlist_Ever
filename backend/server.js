856ef2aa561f84c2a8d15d9058b83796947f1420
Naman1369 <naman.binu.s73@kalvium.community>
2025-04-25 14:53:24 +0530

    mongodb client assignment done

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,23 @@
-const express  = require('express')
+const express = require('express');
+const { connectDB, dbStatus } = require('./database');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000;
 
-app.get('/ping', (req,res) =>{
+connectDB();
+
+app.get('/ping', (req, res) => {
     res.json({
         message: "Pong",
         status: "success",
-    })
-})
+    });
+});
 
-app.get('/', (req,res) => {
-    res.send("<h1>Head to /ping Page!</h1>")
-})
+app.get('/', (req, res) => {
+    res.send(`<h1>MongoDB Status: ${dbStatus()}</h1>`);
+});
 
-app.listen(PORT, () =>{
-    console.log(`Server is running on http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
