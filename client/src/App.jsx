9d8d78c42955bce5b228d9abcd2bc8bf04a36561
Naman1369 <naman.binu.s73@kalvium.community>
2025-05-08 15:15:34 +0530

    frontend created

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>🎧 Worst Playlist Ever</h1>
+      <p>Welcome to the most ear-cringing collection of songs on the internet.</p>
+      <p>This is an ASAP (As Silly As Possible) project where bad taste is the whole point!</p>
+    </div>
+  );
+}
+
+export default App;
