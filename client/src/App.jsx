e28414de6f3aaba2443037fc9ca7eef1e3b17484
Naman1369 <naman.binu.s73@kalvium.community>
2025-05-08 17:09:11 +0530

    bug fixes in component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,18 @@
 import './App.css';
-import SongCard from './components/SongCard';
+import SongCard from './SongCard';
+
+const dummySong = {
+  title: "Cringe Fest 2025",
+  artist: "Off-Key Orchestra",
+  genre: "Unlistenable Pop"
+};
 
 function App() {
-  const dummySong = {
-    title: "The Duck Song",
-    artist: "Unknown YouTuber",
-    genre: "Annoying"
-  };
-
   return (
     <div className="app-container">
       <h1>🎧 Worst Playlist Ever</h1>
       <p>Welcome to the most ear-cringing collection of songs on the internet.</p>
       <p>This is an ASAP (As Silly As Possible) project where bad taste is the whole point!</p>
-
-      <h2 style={{ marginTop: '2rem' }}>🎶 Featured Disaster</h2>
       <SongCard song={dummySong} />
     </div>
   );
--- client/src/components/SongCard.jsx
+++ client/src/components/SongCard.jsx
@@ -1,12 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './App.css';
+
 function SongCard({ song }) {
-    return (
-      <div className="song-card">
-        <h2>{song.title}</h2>
-        <p>Artist: {song.artist}</p>
-        <p>Genre: {song.genre}</p>
-      </div>
-    );
-  }
-  
-  export default SongCard;
-  
+  return (
+    <div className="song-card">
+      <h2>{song.title}</h2>
+      <p>Artist: {song.artist}</p>
+      <p>Genre: {song.genre}</p>
+    </div>
+  );
+}
+
+SongCard.propTypes = {
+  song: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    artist: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default SongCard;
